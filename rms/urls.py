af1e0d96629deef7f376fd4a3fa5c370c63dafb2
Phone Aung <phonexaung@gmail.com>
2023-06-22 22:25:32 +0630

    boostraped log in page and sign up page

--- rms/urls.py
+++ rms/urls.py
@@ -1,20 +1,5 @@
-"""
-URL configuration for rms project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
 
 from core.views import index, about
@@ -23,6 +8,7 @@ from userprofile.views import signup
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
-    path('signup/', signup, name='signup'),
+    path('sign-up/', signup, name='signup'),
+    path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('admin/', admin.site.urls),
 ]
