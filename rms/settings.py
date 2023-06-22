e0537644ca9fcc5e04ee73e8042509afc6733352
Phone Aung <phonexaung@gmail.com>
2023-06-22 22:50:16 +0630

    mod nav bar

--- rms/settings.py
+++ rms/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'index'
 
 # Application definition
 
--- rms/urls.py
+++ rms/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('sign-up/', signup, name='signup'),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
