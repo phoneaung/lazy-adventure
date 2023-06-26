0dc182c9174a7a04dd9db269cc3058cce7792ebd
Phone Aung <phonexaung@gmail.com>
2023-06-26 20:40:18 +0630

    edit candidate detail page

--- candidate/urls.py
+++ candidate/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.candidates_list, name='candidates_list'),
-    path('<int:pk>', views.candidate_details, name='candidate_details'),
+    path('<int:pk>/', views.candidate_details, name='candidate_details'),
     path('add-candidate/', views.add_candidate, name='add_candidate'),
 ]
