b7faf5dae392046cce8c50fffb1038c86b6a12d8
Phone Aung <phonexaung@gmail.com>
2023-07-06 21:42:45 +0630

    add dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from candidate.models import Candidate
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    candidates = Candidate.objects.filter(is_active=True).order_by('-created_at')[0:5]
+    
+    return render(request, 'dashboard/dashboard.html', {
+        'candidates': candidates
+    })
--- rms/urls.py
+++ rms/urls.py
@@ -5,14 +5,13 @@ from django.contrib.auth import views
 from django.urls import path, include
 
 from core.views import index, about
-from userprofile.views import signup
 
 urlpatterns = [
     path('', index, name='index'),
     path('dashboard/', include('dashboard.urls')),
-    path('dashboard/candidates/', include('candidate.urls')),
+    path('candidates/', include('candidate.urls')),
     path('about/', about, name='about'),
-    path('sign-up/', signup, name='signup'),
+    path('', include('userprofile.urls')),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('log-out', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
